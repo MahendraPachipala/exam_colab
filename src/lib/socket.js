7b51cb107445cbd2a97b6abeb6a27be1f723d610
MahendraPachipala <mahendrapachipala123@gmail.com>
2025-04-07 15:06:40 +0530

    socket error fix

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,33 +1,14 @@
 // lib/socket.js
 import { io } from "socket.io-client";
 
-let socket = null;
+const socket = io("https://newsocket-production.up.railway.app/",{
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+  });
 
-export const getSocket = () => {
-    if (!socket && typeof window !== "undefined") {
-        socket = io("https://newsocket-production.up.railway.app/", {
-          reconnection: true,
-          reconnectionAttempts: 5,
-          reconnectionDelay: 1000,
-          reconnectionDelayMax: 5000,
-        });
-      
-        socket.on("connect", () => {
-          console.log("Socket connected:", socket.id);
-        });
-      
-        socket.on("connect_error", (err) => {
-          console.error("Connection error:", err);
-        });
-      
-        socket.on("disconnect", (reason) => {
-          console.warn("Disconnected:", reason);
-        });
-      
-        socket.on("reconnect_attempt", (attempt) => {
-          console.log("Reconnection attempt:", attempt);
-        });
-      }
-      
-  return socket;
-};
+export default socket;
+
+
+
